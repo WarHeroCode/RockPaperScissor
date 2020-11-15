9c30311a7cae493030a040461deae6f83df389d9
Simon Mollina <Simon Mollina@SimonsPC>
2020-11-15 04:33:37 -0600

    Initial Commit

--- RockPaperScissors/public_html/RockPaperScissors.js
+++ RockPaperScissors/public_html/RockPaperScissors.js
@@ -1,35 +1,61 @@
-function generateComputerChoice() {
-//generate a random number between 1 and 3
-//1 = rock, 2 = paper, 3 = scissors
-//the function should return the name of the computer's choice
+document.getElementById('Rock').onclick = user;
+document.getElementById('Paper').onclick = user;
+document.getElementById('Scissors').onclick = user;
+
+
+
+function user(){
+const userChoice = prompt("Do you choose rock, paper or scissors?");
+    console.log("User: " + userChoice);
+
+    var generateComputerChoice = Math.random();
+         if (generateComputerChoice < 0.34) {
+             generateComputerChoice = "Rock";
+          }else if(generateComputerChoice <= 0.67) {
+             generateComputerChoice = "Paper";
+          }else{
+             generateComputerChoice = "Scissors";
+          }; 
+
+    console.log("Computer: " + generateComputerChoice);
+
+(compare(userChoice, generateComputerChoice)); 
+
+var compare = function(choice1,choice2) {
+    if (choice1 === choice2) {
+        return "It's a tie!";
+    }
+    if (choice1 === "rock") {
+        if (choice2 === "scissors") {
+            // rock wins
+            return "You win!";
+        } else {
+            // paper wins
+            return "You lose! Try again.";
+        }
+    }
+    if (choice1 === "paper") {
+        if (choice2 === "rock") {
+            // paper wins
+            return "You win!";
+        } else {
+            // scissors wins
+            return "You lose! Try again.";
+        }
+    }
+    if (choice1 === "scissors") {
+        if (choice2 === "rock") {
+            // rock wins
+            return "You lose! Try again.";
+        } else {
+            // scissors wins
+            return "You win!";
+        }
+    }
+};
+// Run the compare function
+var results = compare(userChoice,generateComputerChoice);
+// Display results
+document.write("<br><hr><b>Results: </b>" + results);
 
 }
-
-function PlayTheGame() {
-//declare variables to hold the player choice and the computer choice
-
-
-
-//use getElementsByName to get the form radio button values
-//loop through the array to find which one is checked
-//store the checked radio button's value in the variable for player choice
-
-
-    
-//Call the generateComputerChoice function and store the returned value
-//in the variable for computer choice
-
-
-
-//compare the player's choice and the computer's choice to determine the winner
-//rock beats scissors, scissors beat paper, paper beats rock
-
-
-
-//use an alert box to tell the player who won, or if it was a tie
-//when displaying the winner, display both the computer choice and the
-//player choice in your message
-
-
-}
-
